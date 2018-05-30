49e6f3e5a18e0894897b193009b686f8be42dea7
Andrew Rob <andrew-c-rob@live.com>
2018-05-30 11:38:43 -0500

    Made progress on stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -7,9 +7,37 @@
 //process: use a for in loop to go into the obj and look for every key and value pair and turn that into a string. Then concat everything together at the end.
 //obj = 9, push input into an array, join it together and return it
 var stringifyJSON = function(obj) {
-    var result = [];
-    for (var i = 0; i < 6; i++){
-        result.push("" + obj[i] + "");
+    // var result = [];
+    // for (var i = 0; i < 6; i++){
+    //     result.push("" + obj[i] + "");
+    // }
+    // return result.join(",");
+    if (typeof obj === 'number' || typeof obj === 'boolean') {
+    	return "" + obj + "";
+    } else if (typeof obj === 'object') {
+    	if (Array.isArray(obj)) {
+    		if (obj.length === 0) {
+    			return '[]';
+    		}
+    		if (obj.length > 0) {
+    			// for (var i = 0; i < obj.length; i++) {
+    			// 	return stringifyJSON(obj[i]);
+    			// }
+    			return '[' + obj.toString() + ']';
+    		}
+
     }
-    return result.join(",");
+    	if (obj === null) {
+    		return "" + obj + "";
+    	}
+    }
+    else if (typeof obj === 'string') {
+    	return '"' + obj + '"';
+    }
+
 };
+
+
+
+//loop
+//stringifyJSON(obj[i])
