4b204163bbdbef6aba1a8a6f4b2cbd0ac14f73bd
Andrew Rob <andrew-c-rob@live.com>
2018-05-30 13:15:49 -0500

    Completed stringifyJSON for recursion

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -7,34 +7,56 @@
 //process: use a for in loop to go into the obj and look for every key and value pair and turn that into a string. Then concat everything together at the end.
 //obj = 9, push input into an array, join it together and return it
 var stringifyJSON = function(obj) {
-    // var result = [];
-    // for (var i = 0; i < 6; i++){
-    //     result.push("" + obj[i] + "");
-    // }
-    // return result.join(",");
-    if (typeof obj === 'number' || typeof obj === 'boolean') {
+	var result = [];
+	var resultObj = [];
+
+	if (typeof obj === 'number' || typeof obj === 'boolean') {
+		return obj.toString();
+	}
+
+	else if (typeof obj === 'string') {
+    	return '"' + obj + '"';
+    } 
+
+    else if (typeof obj === 'object') {
+    	if (obj === null) {
     	return "" + obj + "";
-    } else if (typeof obj === 'object') {
+    }
+
     	if (Array.isArray(obj)) {
     		if (obj.length === 0) {
     			return '[]';
     		}
+
     		if (obj.length > 0) {
-    			// for (var i = 0; i < obj.length; i++) {
-    			// 	return stringifyJSON(obj[i]);
-    			// }
-    			return '[' + obj.toString() + ']';
+    			for (var i = 0; i < obj.length; i++) {
+    				result.push(stringifyJSON(obj[i]));
+    			}
     		}
 
-    }
-    	if (obj === null) {
-    		return "" + obj + "";
+    		return '[' + result + ']';
+    	} 
+    	else {
+//     	    if (typeof obj === 'function' || typeof obj === 'undefined') {
+//               return '{}';
+//             }
+    		if (Object.keys(obj).length === 0) {
+    			return '{}';
+    		}
+    		else if(Object.keys(obj).length > 0) {
+    			for (var key in obj) {
+    			    if (typeof obj[key] === 'function' || typeof obj[key] === 'undefined') {
+    			      return '{}';
+    			    }
+    			    else {
+    				resultObj.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+    			    }
+    			}
+    		}
+    		return"{" + resultObj.toString() + "}";
     	}
-    }
-    else if (typeof obj === 'string') {
-    	return '"' + obj + '"';
-    }
 
+   	}
 };
 
 
