4480c2e8e9e710d9d5959731418d6adfe52872e7
Andrew Rob <andrew-c-rob@live.com>
2018-05-31 19:26:39 -0500

    getElementsByClassName is complete

--- getElementsByClassName.js
+++ getElementsByClassName.js
@@ -1,20 +1,11 @@
-// If life was easy, we could just do things the easy way:
-// var getElementsByClassName = function (className) {
-//   return document.getElementsByClassName(className);
-// };
-
-// But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className) {
-	var element = document.body;
+var getElementsByClassName = function(className, index) {
+	var element = index || document.body;
   	var result = [];
-  	if(element.classList.contains(className)) {
-  		return element;
+  	if(_.contains(element.classList, className)) {
+  		result.push(element);
   	}
-  	//document.body.childNodes provides elements
-  	for(var i = 0; i < element.childNodes; i++) {
-  		if (element.childNodes[i].contains(className)) {
-  			result.push(getElementsByClassName(childNodes[i]));
-  		}
+  	for(var i = 0; i < element.childNodes.length; i++) {
+  		result = result.concat(getElementsByClassName(className, element.childNodes[i]));
   	}
   	return result;
 };
--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,20 +1,11 @@
-// If life was easy, we could just do things the easy way:
-// var getElementsByClassName = function (className) {
-//   return document.getElementsByClassName(className);
-// };
-
-// But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className) {
-	var element = document.body;
-  	var result = [];
-  	if(element.classList.contains(className)) {
-  		return element;
-  	}
-  	//document.body.childNodes provides elements
-  	for(var i = 0; i < element.childNodes; i++) {
-  		if (element.childNodes[i].contains(className)) {
-  			result.push(getElementsByClassName(childNodes[i]));
-  		}
-  	}
-  	return result;
+var getElementsByClassName = function(className, index) {
+    var element = index || document.body;
+     var result = [];
+     if(_.contains(element.classList, className)) {
+         result.push(element);
+     }
+     for(var i = 0; i < element.childNodes.length; i++) {
+             result = result.concat(getElementsByClassName(className, element.childNodes[i]));
+     }
+     return result;
 };
